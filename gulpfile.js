841e01da3938e902b3b1f883346435ad14d220b2
Ben Scott <ben@goodbomb.com>
2014-07-03 11:56:47 -0700

    Added proper sourcemaps to Gulpfile. Also included prod build and server environments.

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var gulp 		 	= require('gulp'),
     streamify		= require('gulp-streamify'),
     uglify 			= require('gulp-uglify'),
     sourcemaps		= require('gulp-sourcemaps'),
-    less 			= require('gulp-less-sourcemap'),
+    less 			= require('gulp-less'),
     prefix			= require('gulp-autoprefixer'),
     minifyCSS 		= require('gulp-minify-css'),
     notify 			= require('gulp-notify'),
@@ -94,7 +94,8 @@ var embedlr 		= require('gulp-embedlr'),
     express 		= require('express'),
     livereload 		= require('connect-livereload'),
     livereloadport 	= 35729,
-    serverport 		= 5000,
+    serverportDev   = 5000,
+    serverportProd  = 5050,
     server 			= express();
 
 server.use(livereload({port: livereloadport}));
@@ -110,9 +111,15 @@ server.all('/*', function(req, res) {
 // Dev Server Task
 // =======================================================================  
 gulp.task('dev', function() {
-	server.listen(serverport);
-	lrserver.listen(livereloadport);
+    server.listen(serverportDev);
+    lrserver.listen(livereloadport);
     console.log('Server running at http://localhost:5000');
+});
+
+gulp.task('stage', function() {
+    server.listen(serverportProd);
+    lrserver.listen(livereloadport);
+    console.log('Server running at http://localhost:5050');
 });
 
 
@@ -136,20 +143,42 @@ gulp.task('lint', function() {
 
 
 // =======================================================================
-// Browserify
+// Browserify Bundle
 // =======================================================================  
-gulp.task('browserify', function() {
+gulp.task('bundle-dev', function() {
     var bundler = watchify(filePath.browserify.src);
 
     bundler.on('update', rebundle)
 
     function rebundle () {
         return bundler.bundle({ debug: true })
+
             .pipe(source('bundle.js'))
             .on("error", handleError)
             .pipe(buffer())
-            // Comment out the next line if you don't want to minify your app in your dev environment. 
+            // Comment out the "Uglify" task if you don't want to minify your app in your dev environment. 
             // However, it can be useful to minify your app periodically to debug any problems with minification.
+            // .pipe(streamify(uglify()))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(filePath.build.dest))
+            .pipe(notify({ message: 'Browserify task complete' }))
+            .pipe(refresh(lrserver));
+    }
+
+    return rebundle()
+});
+
+gulp.task('bundle-prod', function() {
+    var bundler = watchify(filePath.browserify.src);
+
+    bundler.on('update', rebundle)
+
+    function rebundle () {
+        return bundler.bundle({ debug: true })
+            .pipe(sourcemaps.init())
+            .pipe(source('bundle.js'))
+            .on("error", handleError)
+            .pipe(buffer())
             .pipe(streamify(uglify()))
             .pipe(gulp.dest(filePath.build.dest))
             .pipe(notify({ message: 'Browserify task complete' }))
@@ -163,15 +192,28 @@ gulp.task('browserify', function() {
 // =======================================================================
 // Styles Task
 // =======================================================================  
-gulp.task('styles', function () {
+gulp.task('styles-dev', function () {
+    return gulp.src(filePath.styles.src)
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .on("error", handleError)
+        .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"), {map: true})
+    //    .pipe(minifyCSS())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(filePath.build.dest))
+        .on("error", handleError)
+        .pipe(notify({ message: 'Styles task complete' }))
+        .pipe(refresh(lrserver));
+});
+
+gulp.task('styles-prod', function () {
     return gulp.src(filePath.styles.src)
         .pipe(less())
         .on("error", handleError)
-		.pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
-		.pipe(minifyCSS())
+        .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"), {map: true})
+        .pipe(minifyCSS())
         .pipe(gulp.dest(filePath.build.dest))
         .on("error", handleError)
-        // This next line will be displayed twice - once for the CSS file and once for the source map
         .pipe(notify({ message: 'Styles task complete' }))
         .pipe(refresh(lrserver));
 });
@@ -241,8 +283,8 @@ gulp.task('copyFavicon', function () {
 // Watch for changes
 // =======================================================================  
 gulp.task('watch', function () {
-	gulp.watch(filePath.browserify.watch, ['browserify']);
-    gulp.watch(filePath.styles.watch, ['styles']);
+	gulp.watch(filePath.browserify.watch, ['bundle-dev']);
+    gulp.watch(filePath.styles.watch, ['styles-dev']);
     gulp.watch(filePath.images.watch, ['images']);
     gulp.watch(filePath.vendorJS.src, ['vendorJS']);
     gulp.watch(filePath.vendorCSS.src, ['vendorCSS']);
@@ -255,13 +297,23 @@ gulp.task('watch', function () {
 // Sequential Build Rendering
 // =======================================================================  
 gulp.task('build', function(callback) {
-	runSequence(
-	  	['clean', 'lint'],
-		['browserify', 'styles', 'images', 'vendorJS', 'vendorCSS', 'copyIndex', 'copyFavicon'],
-		['dev', 'watch'],
-		callback
-	);
+    runSequence(
+        ['clean', 'lint'],
+        ['bundle-dev', 'styles-dev', 'images', 'vendorJS', 'vendorCSS', 'copyIndex', 'copyFavicon'],
+        ['dev', 'watch'],
+        callback
+    );
+});
+
+gulp.task('prod-build', function(callback) {
+    runSequence(
+        ['clean', 'lint'],
+        ['bundle-prod', 'styles-prod', 'images', 'vendorJS', 'vendorCSS', 'copyIndex', 'copyFavicon'],
+        ['stage'],
+        callback
+    );
 });
 
 
-gulp.task('default',['build']);
+gulp.task('default',['build']); // run "gulp" in terminal to build the development app
+gulp.task('prod',['prod-build']); // run "gulp prod" in terminal to build the prod-ready app
