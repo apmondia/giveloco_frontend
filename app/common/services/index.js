facb0dd19f622bf531ad5a759eea565893d1a1fd
Ben Scott <ben@goodbomb.com>
2014-09-20 15:50:22 -0700

    Revert

--- app/common/services/index.js
+++ app/common/services/index.js
@@ -5,7 +5,7 @@
 
 module.exports = angular.module('common.services', [])
 	.service('alertService', require('./alertService.js'))
-	// .service('apiConfig', require('./apiConfig.js'))
+	.service('apiConfig', require('./apiConfig.js'))
 	.factory('Auth', require('./Auth.js'))
 	.factory('AuthInterceptor', require('./AuthInterceptor.js'))
 	.service('formValidation', require('./formValidation.js'))
