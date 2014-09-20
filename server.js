75473db6ffaa8d7869d500b464590c015642964e
Ben Scott <ben@goodbomb.com>
2014-09-20 16:30:47 -0700

    Heroku setup

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
+var path = require('path');
 
 app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-  res.send('./dist');
+// app.get('/', function(req, res) {
+//   res.send('./dist');
+// });
+
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.static(path.join(__dirname, './dist')));
 });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Server started on " + port);
 });
