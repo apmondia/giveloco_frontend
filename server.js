78bbf86e9a79df7bbf6605016278fa0e3a983478
Ben Scott <ben@goodbomb.com>
2014-09-20 17:13:08 -0700

    Server settings

--- server.js
+++ server.js
@@ -1,5 +1,35 @@
-var express = require('express');
-var port = process.env.PORT || 5000;
+// var express = require("express");
+// var logfmt = require("logfmt");
+// var app = express();
+// var path = require('path');
+
+// app.use(logfmt.requestLogger());
+
+// app.get('/', function(req, res) {
+//   res.send('./dist');
+// });
+
+// app.configure(function(){
+//     app.use(express.bodyParser());
+//     app.use(express.static(path.join(__dirname, './dist')));
+// });
+
+// var port = Number(process.env.PORT || 5000);
+// app.listen(port, function() {
+//   console.log("Server started on " + port);
+// });
+
+var express = require("express");
+var logfmt = require("logfmt");
 var app = express();
-app.use(express.static(__dirname + './dist'));
-app.listen(port);
+
+app.use(logfmt.requestLogger());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
