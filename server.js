1b4c2eae1d5d549f2a47d0b0ac05a7790e191e64
Ben Scott <ben@goodbomb.com>
2014-09-20 17:07:45 -0700

    attempting new server approach

--- server.js
+++ server.js
@@ -1,20 +1,5 @@
-var express = require("express");
-var logfmt = require("logfmt");
+var express = require('express');
+var port = process.env.PORT || 5000;
 var app = express();
-var path = require('path');
-
-app.use(logfmt.requestLogger());
-
-app.get('/', function(req, res) {
-  res.send('./dist');
-});
-
-app.configure(function(){
-    app.use(express.bodyParser());
-    app.use(express.static(path.join(__dirname, './dist')));
-});
-
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function() {
-  console.log("Server started on " + port);
-});
+app.use(express.static(__dirname + './dist'));
+app.listen(port);
