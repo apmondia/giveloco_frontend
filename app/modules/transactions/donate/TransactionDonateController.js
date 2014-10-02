e1c2cefb89ff2226f11ccc30acb73121706ff32b
Ben Scott <ben@goodbomb.com>
2014-10-02 12:18:06 -0700

    Completed donation transactions

--- app/modules/transactions/donate/TransactionDonateController.js
+++ app/modules/transactions/donate/TransactionDonateController.js
@@ -4,19 +4,26 @@ function TransactionDonateCtrl($rootScope, $scope, TRANSACTION_EVENTS, alertServ
 
 	var currentUser = $scope.currentUser;
 
+	$scope.name = $scope.currentUser.first_name + ' ' + $scope.currentUser.last_name;
+	
 	$scope.transaction = {
 		stripeToken: null,
 		amount: null,
-		name: currentUser.first_name + ' ' + currentUser.last_name,
 		from_user_id: currentUser.id,
 		to_user_id: $scope.user.id
 	};
 
-	console.log($scope.currentUser.id);
-	console.log($scope.user.id);
+	var resetForm = function() {
+		$scope.transaction.amount = '';
+		$scope.number = '';
+		$scope.expiry = '';
+		$scope.cvc = '';
+	};
 
 	var transactionSuccess = function() {
 		alertService.showAlert(TRANSACTION_EVENTS.donationSuccess, 'alert-success');
+		resetForm();
+		$scope.donationForm.$setPristine();
 	};
 
 	var transactionError = function() {
@@ -24,14 +31,10 @@ function TransactionDonateCtrl($rootScope, $scope, TRANSACTION_EVENTS, alertServ
 	};
 
 	$scope.stripeCallBack = function (status, response) {
-
 		if (response.error) {
 			alertService.showAlert(TRANSACTION_EVENTS.paymentFailure, 'alert-danger');
-			console.log('it failed! error: ' + response.error.message);
 		} else {
-			console.log('success! token: ' + response.id);
 			$scope.transaction.stripeToken = response.id;
-			$scope.transaction.amount *= 100; // Transaction amount has to be in cents, not dollars
 			TransactionService.donation($scope.transaction).then(transactionSuccess, transactionError);
 		}
 	};
