8085ff803c6f6dd6d234729ce6fc68706345d285
Brendan Asselstine <mail.asselstine@gmail.com>
2014-12-23 10:25:19 -0800

    Added some clearer error display to the user profile tags entry.

--- app/modules/users/account/details/edit/usersAccountDetailsEditDirective.js
+++ app/modules/users/account/details/edit/usersAccountDetailsEditDirective.js
@@ -5,6 +5,18 @@ module.exports = function usersAccountDetailsEditDirective() {
 		controller: 'UsersAccountDetailsEditCtrl',
 		template: require('./users-account-details-edit.html'),
 		restrict: 'A',
-		scope: false
+		scope: false,
+		link: function(scope, element, attrs) {
+			$(element).on('focus','#userTags', function () {
+				scope.updateUserForm.tags.$focused = true;
+			});
+			$(element).on('blur', '#userTags', function () {
+				scope.updateUserForm.tags.$focused = false;
+			});
+			scope.$on('$destroy', function () {
+				angular.element('#userTags').unbind('focus');
+				angular.element('#userTags').unbind('blur');
+			});
+		}
 	};
 };
