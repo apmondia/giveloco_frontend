187207290bf2ac860be251c461ddc1adb1ba3a00
Brendan Asselstine <mail.asselstine@gmail.com>
2015-01-14 22:40:05 -0800

    Nearly completed the design of the 'As a user I want to enter my gift card'

--- app/modules/users/profile/business/purchase/UsersProfileBusinessPurchaseController.js
+++ app/modules/users/profile/business/purchase/UsersProfileBusinessPurchaseController.js
@@ -2,7 +2,7 @@
 
 function UsersProfileBusinessPurchaseCtrl($scope, TRANSACTION_EVENTS, alertService, Restangular, SponsorService, TransactionService) {
 
-	$scope.amountRegex = /\$?(\d+(\.\d+)?)/;
+	$scope.amountRegex = /^\$?(\d+(\.\d+)?)$/;
 
 /* =======================================================================
 	Step 1
@@ -16,40 +16,28 @@ function UsersProfileBusinessPurchaseCtrl($scope, TRANSACTION_EVENTS, alertServi
 		agree_to_tc: false,
 		certificates_attributes: {
 			sponsorship_id: null,
-			amount: 20,
+			amount: null,
 			serial_number: null
 		}
 	};
 
 	$scope.newUser = angular.copy(EMPTY_NEW_USER);
 
+	$scope.calculateDonationAmount = function () {
+		return parseFloat($scope.newUser.certificates_attributes.amount) *
+			(parseFloat($scope.user.sponsorship_rate) / 100.0);
+	};
+
   $scope.stepOneNotComplete = function() {
 		return $scope.registrationForm.serial_number.$invalid ||
 						$scope.registrationForm.amount.$invalid;
   };
 
-
-	/* =======================================================================
-		Step 2
-	======================================================================= */
-	// Restructured sponsorship object to join the Cause ID with its Sponsorship ID for form submission
-	$scope.sponsorshipData = function() {
-		var sponsorships = SponsorService.getAcceptedSponsorships($scope.user);
-
-		var sponsorshipData = [];
-		for (var i=0; i < sponsorships.length; i++) {
-			var getCause = Restangular.one('users', sponsorships[i].cause.id).get().$object;
-			sponsorshipData.push({
-				id: sponsorships[i].id,
-				percentage: sponsorships[i].donation_percentage,
-				cause: getCause
-			});
-		}
-		return sponsorshipData;
-	};
-
-	// set sponsorships model
-	$scope.sponsorships = $scope.sponsorshipData();
+	SponsorService.getSponsoredCauses($scope.user).then(function (data) {
+		$scope.sponsorships = _.filter(data, function (sponsorship) {
+			return sponsorship.status == 'accepted';
+		});
+	});
 
 	$scope.stepTwoNotComplete = function() {
 		return $scope.newUser.certificates_attributes.sponsorship_id === null ? true : false;
@@ -69,7 +57,7 @@ function UsersProfileBusinessPurchaseCtrl($scope, TRANSACTION_EVENTS, alertServi
     });
 
     $scope.stepThreeNotComplete = function() {
-		return $scope.newUser.agree_to_tc === false ? true : false;
+			return $scope.registrationForm.agree_to_tc.$invalid || $scope.registrationForm.email.$invalid;
     };
 
     // reset form when the modal is closed or competed submission
@@ -78,23 +66,32 @@ function UsersProfileBusinessPurchaseCtrl($scope, TRANSACTION_EVENTS, alertServi
 		$scope.registrationForm.$setPristine();
 	};
 
-	var transactionSuccess = function() {
-		alertService.showAlert(TRANSACTION_EVENTS.transactionSuccess, 'alert-success');
+	$scope.closeRegistrationForm = function () {
+		$scope.usersProfileBusinessPurchaseModal.close();
 		resetForm();
+	};
+
+	var transactionSuccess = function() {
+		$scope.closeRegistrationForm();
+		alertService.showAlert(TRANSACTION_EVENTS.transactionSuccess, 'alert-success');
 	};
 
 	var transactionError = function() {
 		alertService.showAlert(TRANSACTION_EVENTS.transactionFailure, 'alert-danger');
 	};
 
-	$scope.registerCertificate = function (event) {
-		var data = {
-			newUser: angular.copy($scope.newUser)
-		};
-		var amountMatch = data.newUser.certificates_attributes.amount.match( $scope.amountRegex );
-		data.newUser.certificates_attributes.amount = amountMatch[1];
-		data.newUser.certificates_attributes = [ data.newUser.certificates_attributes ];
-		TransactionService.purchaseCertificate(data).then(transactionSuccess, transactionError);
+	$scope.registerCertificate = function (valid) {
+		if ($scope.registrationForm.$valid) {
+			var data = {
+				newUser: angular.copy($scope.newUser)
+			};
+			var amountMatch = data.newUser.certificates_attributes.amount.match( $scope.amountRegex );
+			data.newUser.certificates_attributes.amount = amountMatch[1];
+			data.newUser.certificates_attributes = [ data.newUser.certificates_attributes ];
+			TransactionService.purchaseCertificate(data).then(transactionSuccess, transactionError);
+		} else {
+			$scope.submitAttempted = true;
+		}
 	};
 
 }
