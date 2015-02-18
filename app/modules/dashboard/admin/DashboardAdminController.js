5b65b3da99b88d155296c078127dc680357d57f3
Brendan Asselstine <mail.asselstine@gmail.com>
2015-02-18 10:15:06 -0800

    Added edit profile and activate / deactivate capabilities to the admin cause dashboard

--- app/modules/dashboard/admin/DashboardAdminController.js
+++ app/modules/dashboard/admin/DashboardAdminController.js
@@ -108,6 +108,28 @@ function DashboardAdminCtrl($scope, Restangular, alertService) {
 				});
 			};
 
+			$scope.deactivateUser = function(user) {
+				var original = user.is_activated;
+				user.is_activated = false;
+				user.save().then(function () {
+					//alert("Deactivated user!");
+				}, function () {
+					//alert("Could NOT deactivate user!");
+					user.is_activated = original;
+				});
+			};
+
+			$scope.activateUser = function(user) {
+				var original = user.is_activated;
+				user.is_activated = true;
+				user.save().then(function () {
+					//alert("Activated user");
+				}, function () {
+					//alert("Could not activate user");
+					user.is_activated = original;
+				});
+			};
+
 }
 
 DashboardAdminCtrl.$inject = ['$scope', 'Restangular', 'alertService'];
--- app/modules/dashboard/admin/tab-businesses/DashboardAdminTabBusinessesController.js
+++ app/modules/dashboard/admin/tab-businesses/DashboardAdminTabBusinessesController.js
@@ -1,27 +1,7 @@
 'use strict';
 
 function DashboardAdminTabBusinessesCtrl($scope) {
-  $scope.deactivateUser = function(user) {
-    var original = user.is_activated;
-    user.is_activated = false;
-    user.save().then(function () {
-      //alert("Deactivated user!");
-    }, function () {
-      //alert("Could NOT deactivate user!");
-      user.is_activated = original;
-    });
-  };
-
-  $scope.activateUser = function(user) {
-    var original = user.is_activated;
-    user.is_activated = true;
-    user.save().then(function () {
-      //alert("Activated user");
-    }, function () {
-      //alert("Could not activate user");
-      user.is_activated = original;
-    });
-  };
+  
 }
 
 DashboardAdminTabBusinessesCtrl.$inject = ['$scope'];
