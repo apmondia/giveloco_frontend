ec73a0efb2efaddb16c9d3863626509570a2635b
Brendan Asselstine <mail.asselstine@gmail.com>
2015-03-06 12:41:22 -0800

    Changed 'Thank you for giving it forward' to 'Thank you for spending it forward'

--- app/common/constants/TRANSACTION_EVENTS.js
+++ app/common/constants/TRANSACTION_EVENTS.js
@@ -4,7 +4,7 @@ var TRANSACTION_EVENTS = (function() {
     return {
     	paymentFailure: 'Uh Oh! Something went wrong. Your credit card information may not be valid.',
 		transactionFailure: 'Uh Oh! Something went wrong. Please check that your information is correct and try again.',
-		transactionSuccess: 'Thank you for giving it forward!',
+		transactionSuccess: 'Thank you for spending it forward!',
 		redemptionSuccess: 'You have successfully redeemed the gift certificate',
 		redemptionFailure: 'We could not redeem that gift certificate'
     };
